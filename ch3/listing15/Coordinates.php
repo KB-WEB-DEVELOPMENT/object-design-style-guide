3cf8c36755f54f4cba4940497ff960cd4a8bb481
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-01-26 23:30:57 +0100

    Create Coordinates.php

---
+++ ch3/listing15/Coordinates.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+declare(strict_types=1);
+
+namespace MyClasses;
+
+final class Coordinates
+{
+	// ...
+	public function __construct(
+		private float $latitude, 
+		private float $longitude
+	){
+
+		if ($this->latitude > 90 || $this->latitude < -90) {
+			throw new InvalidArgumentException('Latitude should be between -90 and 90');
+		}
+	
+		if (longitude > 180 || longitude < -180) {
+			throw new InvalidArgumentException('Longitude should be between -180 and 180');
+		}
+	
+	}
+
+	// ...
+}
