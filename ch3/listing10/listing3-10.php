d1ee35333e3bdee4a9846d19bf6db763b19e9d1c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-01-26 23:26:13 +0100

    Create listing3-10.php

---
+++ ch3/listing10/listing3-10.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Solution : Listing 3.10 here
+Check previous listings 3.8 and listing 3.9
+The idea is to remove superfluous constructor arguments
+*/
+final class Deal
+{
+
+	public function __construct(
+		private int $amountToFirstParty,
+		private int $amountToSecondParty
+	) {
+		if ($this->amountToFirstParty <= 0) {
+			throw new InvalidArgumentException(/* ... */);
+		}
+
+		if ($this->amountToSecondParty <= 0) {
+			throw new InvalidArgumentException(/* ... */);
+		}
+	}
+
+	public function totalAmount(): int
+	{
+		return $this->amountToFirstParty + $this->amountToSecondParty;
+	}
+}
