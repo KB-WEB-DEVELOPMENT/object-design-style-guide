31ae66fbf2ea20281a1fef3042c41a4d8591177f
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-01-27 00:54:40 +0100

    Create listing7-1.php

---
+++ ch7/listing1/listing7-1.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Listing 7.1: For command methods, use methods with imperative names
+*/
+
+public function sendReminderEmail(EmailAddress $recipient,/* ... */): void 
+{
+	// ...
+}
+public function saveRecord(Record record): void
+{
+	// ...
+}
