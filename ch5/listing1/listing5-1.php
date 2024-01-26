2e0d9468ceb160c063835659e4b896d9e4595823
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-01-27 00:31:08 +0100

    Create listing5-1.php

---
+++ ch5/listing1/listing5-1.php
@@ -0,0 +1,19 @@
+<?php
+
+// Listing 5.1: Basic template for implementing in PHP OOP
+
+/*
+
+	[scope] function methodName(type name, ...): void|[return-type]
+	{
+		[preconditions checks]
+
+		[failure scenarios]
+
+		[happy path]
+
+		[postcondition checks]
+
+		[return void|specific-return-type]
+
+*/
