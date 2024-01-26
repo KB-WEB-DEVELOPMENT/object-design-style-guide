526d43321d5a0b474f7d497d679225c67b935f00
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-01-26 22:58:11 +0100

    Add files via upload

---
+++ ch2/listing21/listing2-21.php
@@ -0,0 +1,10 @@
+<?php
+
+// EntityManager which saves only a single object at a time - not very useful
+
+$user = new User(/* ... */);
+$comment = new Comment(/* ... */);
+$entityManager = new EntityManager($user);
+$entityManager.save();
+$entityManager = new EntityManager($comment);
+$entityManager.save();
