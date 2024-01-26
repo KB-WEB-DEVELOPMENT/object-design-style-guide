52efcc8d06cc51b6030750c5113842eca1d7755e
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-01-26 22:59:31 +0100

    Create listing2-23.php

---
+++ ch2/listing23/listing2-23.php
@@ -0,0 +1,10 @@
+<?php
+
+// Compare with listings 2.21 and 2.22 - better alternative : Entity should be passed as method argument
+final class EntityManager
+{
+	public function save(object $entity): void
+	{
+		// ...
+	}
+}
