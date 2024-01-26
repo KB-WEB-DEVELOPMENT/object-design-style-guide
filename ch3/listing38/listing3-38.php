221d49d32c8d0af4da5dfecde67f89837aee34f1
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-01-26 23:50:33 +0100

    Create listing3-38.php

---
+++ ch3/listing38/listing3-38.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+listing 3.38 & 3.39 :
+- Using a property filler for the ScheduleMeetup DTO
+- Validating the ScheduleMeetup DTO
+- Also look at listing 3.36 & 3.37
+*/
+
+final class ScheduleMeetup
+{
+	public string $title;
+	public string $datum;
+	
+	public static function fromFormData(array $formData): ScheduleMeetup
+	{		
+		$scheduleMeetup = new self();
+		$scheduleMeetup->title = $formData['title'];
+		$scheduleMeetup->datum = $formData['date'];
+
+		return $scheduleMeetup;
+	}
+	
+	#  use form and validation libraries , ex: Symfony Form and Validator components 
+	#  The method is fictitious, it serves only as an example.
+	public function validate(): array
+	{
+		$errors = [];
+
+		if ($this->title == '') {		
+			$errors['title'][] = 'validation.empty_title';
+		}
+		
+		if (this.date == '') {
+			$errors['date'][] = 'validation.empty_date';
+		}
+		
+		DateTime::createFromFormat('d/m/Y',$this->datum);
+
+		$errors = DateTime::getLastErrors();
+
+		if ($errors['error_count'] > 0) {
+			errors['date'][] = 'validation.invalid_date_format';
+		}
+	
+		return $errors;
+	}
+}
