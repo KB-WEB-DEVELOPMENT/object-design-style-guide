57c6719e8589f9924bc754dd4f024e8ce29e2b4c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-01-26 22:16:15 +0100

    Create Bar.php

---
+++ ch1/listing19/Bar.php
@@ -0,0 +1,10 @@
+<?php
+
+// Bar class extends its parent class Foo and modifies the behavior of Foo bar() method
+class Bar extends Foo
+{
+	public function bar(): void
+	{
+		// do something else
+	}
+}
