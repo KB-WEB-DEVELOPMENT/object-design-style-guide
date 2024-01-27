05186d402183198bcbd586b2663766409a103a19
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-01-27 01:18:56 +0100

    Update listing8-10.php

--- ch8/listing10/listing8-10.php
+++ ch8/listing10/listing8-10.php
@@ -16,11 +16,10 @@ public function execute(Request $request): Response
 	$allPurchaseOrders = $this->repository->findAll();
 
 	$forStockReport = array_map(
-						function (PurchaseOrder $purchaseOrder) {
-							return $purchaseOrder->forStockReport();
-						},
-						$allPurchaseOrders
-					);
+			     function (PurchaseOrder $purchaseOrder) {
+			         return $purchaseOrder->forStockReport();
+				},$allPurchaseOrders
+			  );
 	// ...
 }
 
