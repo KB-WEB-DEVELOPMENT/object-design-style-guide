8675ae2dd7b2e0e20b749848a9c49482dec7f7e0
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-02-18 19:19:59 +0100

    Update listing2-21.php

--- ch2/listing21/listing2-21.php
+++ ch2/listing21/listing2-21.php
@@ -5,6 +5,6 @@
 $user = new User(/* ... */);
 $comment = new Comment(/* ... */);
 $entityManager = new EntityManager($user);
-$entityManager.save();
+$entityManager->save();
 $entityManager = new EntityManager($comment);
-$entityManager.save();
+$entityManager->save();
